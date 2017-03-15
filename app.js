73f8880854b13b908afe9be95a7111b67185565f
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2017-03-15 16:28:43 -0300

    Finalizando conceitos básicos

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var app = new Vue({
 	el: "#app",
 	data: {
+		test: '',
+		formType: '',
 		title: "Contas a pagar",
 		menus: [
 			{id:0, name:"Listar contas"},
 			{id:1, name:"Criar conta"}
 		],
-		activedView: 0,
+		activedView: 1,
+		bill: {
+			date_due: '',
+			name: '',
+			value: 0,
+			done: 0
+		},
+		names:[
+			'Conta de Luz',
+			'Conta de Água',
+			'Conta de Internet',
+			'Conta de Cartão',
+			'Conta de Aluguel',
+		],
 		bills: [
 			{date_due: '20/08/2015', name: 'Conta de Luz', value: 25.99, done:1},
 			{date_due: '23/08/2015', name: 'Conta de Água', value: 22.99, done:0},
@@ -29,6 +44,34 @@ var app = new Vue({
 	methods: {
 		showView: function(id){
 			this.activedView = id;
+			if(id == 1){
+				this.formType = 'insert';
+			}
+		},
+		submit: function(){
+			if(this.formType == 'insert'){
+				this.bills.push(this.bill);
+			}
+			this.bill = {
+				date_due: '',
+				name: '',
+				value: 0,
+				done: 0
+			};
+			this.activedView = 0;
+		},
+		loadBill: function(bill){
+			this.bill = bill;
+			this.activedView = 1;
+			this.formType = 'update';
 		}
+	}
+});
+
+Vue.filter('doneLabel', function(value){
+	if(value == 0){
+		return "Não Paga";
+	}else{
+		return "Paga";
 	}
 });
